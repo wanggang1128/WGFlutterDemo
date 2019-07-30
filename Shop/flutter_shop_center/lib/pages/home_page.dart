33ccb70622fa1577af02b461304273511d3e1222
王刚 <wanggang@317hu.com>
2019-07-30 09:58:39 +0800

    网格组件，广告组件

--- Shop/flutter_shop_center/lib/pages/home_page.dart
+++ Shop/flutter_shop_center/lib/pages/home_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,8 +16,6 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
 
-    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
-
     return Container(
       child: Scaffold(
         appBar: AppBar(title: Text('网络数据请求'),),
@@ -24,10 +24,17 @@ class _HomePageState extends State<HomePage> {
             builder: (context, snapShort){
               if(snapShort.hasData){
                 var data = json.decode(snapShort.data.toString());
+                //轮播图
                 List<Map> swiper = (data['data']['slides'] as List).cast();
+                //网格
+                List<Map> topNavigator = (data['data']['category'] as List).cast();
+                //广告
+                String adPicUrl = data['data']['advertesPicture']['PICTURE_ADDRESS'];
                 return Column(
                   children: <Widget>[
-                    SwiperDiy(swiperDataList: swiper,)
+                    SwiperDiy(swiperDataList: swiper,),
+                    TopNavigator(navigatorList: topNavigator,),
+                    AdBanner(adPicUrl: adPicUrl,)
                   ],
                 );
               }else{
@@ -61,6 +68,60 @@ class SwiperDiy extends StatelessWidget {
         pagination: SwiperPagination(),
         autoplay: true,
       ),
+    );
+  }
+}
+
+//网格组件
+class TopNavigator extends StatelessWidget {
+
+  final List navigatorList;
+  TopNavigator({Key key, this.navigatorList}):super(key:key);
+
+  Widget _gridViewItem(BuildContext context, item){
+    return InkWell(
+      onTap: (){},
+      child: Column(
+        children: <Widget>[
+          Image.network(item['image'],width: ScreenUtil().setWidth(95),),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    //这里只需要10个，为了好看而已
+    if(navigatorList.length>10){
+      navigatorList.removeRange(10, navigatorList.length);
+    }
+
+    return Container(
+      height: ScreenUtil().setHeight(270),
+      padding: EdgeInsets.all(5.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(5.0),
+        children: navigatorList.map((item){
+          return _gridViewItem(context, item);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+//广告组件
+class AdBanner extends StatelessWidget {
+
+  final String adPicUrl;
+  AdBanner({this.adPicUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network(adPicUrl),
     );
   }
 }
--- Shop/flutter_shop_center/lib/pages/index_page.dart
+++ Shop/flutter_shop_center/lib/pages/index_page.dart
@@ -8,6 +8,9 @@ import 'cart_page.dart';
 import 'category_page.dart';
 import 'member_page.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
 class IndexPage extends StatefulWidget {
   @override
   _IndexPageState createState() => _IndexPageState();
@@ -55,6 +58,9 @@ class _IndexPageState extends State<IndexPage> {
 
   @override
   Widget build(BuildContext context) {
+
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
       bottomNavigationBar: BottomNavigationBar(
