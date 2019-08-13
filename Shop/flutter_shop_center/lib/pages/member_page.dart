36aa796cb5adff0e5a27d0e866d0ea25992c7ab0
王刚 <wanggang@317hu.com>
2019-08-13 16:21:56 +0800

    修改人：王刚  修改内容：会员中心

--- Shop/flutter_shop_center/lib/pages/member_page.dart
+++ Shop/flutter_shop_center/lib/pages/member_page.dart
@@ -1,18 +1,172 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:provide/provide.dart';
-import '../provide/counter.dart';
 
 class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Provide<Counter>(
-            builder: (context, child, counter) {
-              return Text('${counter.value}', style: TextStyle(fontSize: 30),);
-            }
-        ),
+      appBar: AppBar(
+        title: Text('会员中心'),
+      ),
+      body:ListView(
+        children: <Widget>[
+          _topHeader(),
+          _orderTitle(),
+          _orderType(),
+          _actionList()
+        ],
+      ) ,
+    );
+  }
+
+
+  //头部组件
+  Widget _topHeader(){
+
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: Colors.pinkAccent,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: ClipOval(
+                child:Image.network(
+                  'https://upload.jianshu.io/users/upload_avatars/5720820/a8379ec9-dbe8-4a45-bf97-52905d531156.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/100/h/100',
+                )
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              '寒江',
+              style: TextStyle(
+                fontSize: ScreenUtil().setSp(36),
+                color:Colors.white,
+
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  //我的订单顶部
+  Widget _orderTitle(){
+
+    return Container(
+      margin: EdgeInsets.only(top:10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+              bottom:BorderSide(width: 1,color:Colors.black12)
+          )
+      ),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title:Text('我的订单'),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+
+  Widget _orderType(){
+    return Container(
+      margin: EdgeInsets.only(top:5),
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.only(top:20),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.party_mode,
+                  size: 30,
+                ),
+                Text('待付款'),
+              ],
+            ),
+          ),
+          //-----------------
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.query_builder,
+                  size: 30,
+                ),
+                Text('待发货'),
+              ],
+            ),
+          ),
+          //-----------------
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.directions_car,
+                  size: 30,
+                ),
+                Text('待收货'),
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.content_paste,
+                  size: 30,
+                ),
+                Text('待评价'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _myListTile(String title){
+
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+              bottom:BorderSide(width: 1,color:Colors.black12)
+          )
+      ),
+      child: ListTile(
+        leading: Icon(Icons.blur_circular),
+        title: Text(title),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+  Widget _actionList(){
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          _myListTile('领取优惠券'),
+          _myListTile('已领取优惠券'),
+          _myListTile('地址管理'),
+          _myListTile('客服电话'),
+          _myListTile('关于我们'),
+        ],
       ),
     );
   }
